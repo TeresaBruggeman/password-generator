6e32e916db983ab47f5c92db1e440256198797b5
TBruggeman <teresabrugg4@gmail.com>
2020-02-29 21:24:13 -0700

    removing extraneous code

--- script.js
+++ script.js
@@ -2,50 +2,44 @@
 var generateBtn = document.querySelector("#generate");
 
 // Create password
-// get length of password from user
 function generatePassword() {
   var pwLength = genPasswordLength();  //call function to get password length from user
   var charType = [];
   var charAvail = ["UPPERCASE", "lowercase", "numb3r5", "$peci@l"];
+ 
   //loop to accept input until charAvail array has been exhausted
   for (var i = 0; i < charAvail.length; i++) {
     var userChoice = confirm("Confirm if " + charAvail[i] + " should be included in new password.");
-    localStorage.setItem.userChoice;
-    console.log(userChoice);
+    
     //populate charType array with user's choices
     if (userChoice === true) {
       charType.push(charAvail[i]);
-      console.log(charType);
     }
-    //if they don't choose any character types end program
+    
+    //if no character types chosen end program
     else if (userChoice === false && charAvail[i] === charAvail[charAvail.length - 1] && !charType.length) {
       alert("Please try again");
       return;
     }
-    else { 
-    }
   }
+
   //generating password
   var randomChar = "";
   var newPassword = "";
+  
   while (newPassword.length < pwLength) {
     randomCharSet = charType[(Math.floor(Math.random() * charType.length))];
-    console.log(randomCharSet);
-    if (randomCharSet === "UPPERCASE") {
+        if (randomCharSet === "UPPERCASE") {
       randomChar = ("ABCDEFGHIJKLMNOPQRSTUVWXYZ").split('')[(Math.floor(Math.random() * 26))];
-      console.log(randomChar);
     }
     else if (randomCharSet === "lowercase") {
       randomChar = ("abcdefghijklmnopqrstuvwxyz").split('')[(Math.floor(Math.random() * 26))];
-      console.log(randomChar);
     }
     else if (randomCharSet === "numb3r5") {
       randomChar = ("1234567890").split('')[(Math.floor(Math.random() * 10))];
-      console.log(randomChar);
     }
     else if (randomCharSet === "$peci@l") {
       randomChar = ("!@#%^&*()+-=:").split('')[(Math.floor(Math.random() * 13))];
-      console.log(randomChar);
     }
     newPassword += randomChar;
     randomChar = "";
@@ -55,11 +49,11 @@ function generatePassword() {
 //prompts user to specify how many characters long the password should be, returns numerics length
 function genPasswordLength() {
   var pwLength = prompt("Please enter the number of characters the new password should contain. (Enter a number between 8-128)");
-  parseInt(pwLength);
+  
+  //verify entry was within the range
   while (pwLength < 8 || pwLength > 128) {
     pwLength = prompt("Entry invalid. Please enter within the range of 8 - 128.");
   }
-  console.log(pwLength);
   return pwLength;
 }
   // Write password to the #password input
